21b15b201493c265c7a9e4c0563cda2c02dd1e49
caiomello <github@caiomello.com>
2021-11-10 18:07:02 +0100

    Create package

---
+++ Sources/Notifications/NotificationService.swift
@@ -0,0 +1,135 @@
+//
+//  NotificationService.swift
+//  
+//
+//  Created by Caio Mello on 10.11.21.
+//
+
+import Foundation
+import UserNotifications
+import UniformTypeIdentifiers
+
+public protocol NotificationServiceDelegate {
+    var notificationsEnabled: Bool { get }
+    var notificationTime: Int { get }
+}
+
+public struct NotificationContent {
+    let imageFilePath: String?
+    let title: String
+    let subtitle: String?
+    let body: String?
+    let date: Date
+    let identifier: String
+    let userInfo: [AnyHashable: Any]
+
+    public init(imageFilePath: String?, title: String, subtitle: String?, body: String?, date: Date, identifier: String, userInfo: [AnyHashable: Any]) {
+        self.imageFilePath = imageFilePath
+        self.title = title
+        self.subtitle = subtitle
+        self.body = body
+        self.date = date
+        self.identifier = identifier
+        self.userInfo = userInfo
+    }
+}
+
+public struct NotificationService {
+    private let delegate: NotificationServiceDelegate
+    private let debugMode: Bool
+
+    public init(delegate: NotificationServiceDelegate, debugMode: Bool = false) {
+        self.delegate = delegate
+        self.debugMode = debugMode
+    }
+}
+
+// MARK: - Operations
+
+extension NotificationService {
+    public func requestAuthorization(_ completion: @escaping (Bool) -> Void) {
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge]) { (granted, error) in
+            completion(granted)
+        }
+    }
+
+    public func getAuthorizationStatus(completion: @escaping (UNAuthorizationStatus) -> Void) {
+        UNUserNotificationCenter.current().getNotificationSettings { settings in
+            completion(settings.authorizationStatus)
+        }
+    }
+
+    public func scheduleNotifications(withContent content: [NotificationContent]) {
+        if self.delegate.notificationsEnabled {
+            content.forEach({ self.scheduleNotification(withContent: $0) })
+        }
+    }
+
+    private func scheduleNotification(withContent content: NotificationContent) {
+        let notification = UNMutableNotificationContent()
+        notification.title = content.title
+        notification.userInfo = content.userInfo
+
+        if let subtitle = content.subtitle {
+            notification.subtitle = subtitle
+        }
+
+        if let body = content.body {
+            notification.body = body
+        }
+
+        if let imagePath = content.imageFilePath, let attachment = imageAttachment(withPath: imagePath) {
+            notification.attachments = [attachment]
+        }
+
+        let components: DateComponents = {
+            if debugMode {
+//                Test components (fire 2s from now)
+                return Calendar.current.dateComponents([.second, .minute, .hour, .day, .month, .year], from: Date().addingTimeInterval(2))
+            } else {
+                var components = Calendar.current.dateComponents([.hour, .day, .month, .year], from: content.date)
+                components.hour = delegate.notificationTime
+                return components
+            }
+        }()
+
+        let trigger = UNCalendarNotificationTrigger(dateMatching: components, repeats: false)
+
+        let request = UNNotificationRequest(identifier: content.identifier, content: notification, trigger: trigger)
+
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: { error in
+            if let error = error {
+                print("Error scheduling notification: \(error)")
+            }
+        })
+    }
+
+    public func cancelNotifications(withIdentifiers identifiers: [String]) {
+        UNUserNotificationCenter.current().removePendingNotificationRequests(withIdentifiers: identifiers)
+    }
+
+    public func cancelAllNotifications() {
+        UNUserNotificationCenter.current().removeAllPendingNotificationRequests()
+    }
+
+    // Helpers
+
+    private func imageAttachment(withPath path: String) -> UNNotificationAttachment? {
+        let originalFileURL = URL(fileURLWithPath: path)
+        let copyFolderURL = FileManager.default.temporaryDirectory.appendingPathComponent(ProcessInfo.processInfo.globallyUniqueString, isDirectory: true)
+        let copyFileURL = copyFolderURL.appendingPathComponent(originalFileURL.lastPathComponent)
+
+        do {
+            try FileManager.default.createDirectory(atPath: copyFolderURL.path, withIntermediateDirectories: true, attributes: nil)
+            try FileManager.default.copyItem(atPath: originalFileURL.path, toPath: copyFileURL.path)
+
+            return try UNNotificationAttachment(identifier: "image",
+                                                url: copyFileURL,
+                                                options: [UNNotificationAttachmentOptionsTypeHintKey: UTType.jpeg])
+
+        } catch {
+            print("Error creating notification attachment: \(error)")
+            return nil
+        }
+    }
+}
---
+++ Tests/NotificationsTests/NotificationsTests.swift
@@ -0,0 +1,6 @@
+import XCTest
+@testable import Notifications
+
+final class NotificationsTests: XCTestCase {
+
+}
